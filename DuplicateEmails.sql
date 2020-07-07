b7830928ceea58ad46de95850e4b08535f786d35
swetavooda <48858628+swetavooda@users.noreply.github.com>
2020-07-07 22:04:56 +0530

    DuplicateEmails (GROUP BY and HAVING)
    GROUP BY and HAVING (aggregate functions)

--- DuplicateEmails.sql
+++ DuplicateEmails.sql
@@ -1,2 +1,7 @@
+#problem link: https://leetcode.com/problems/duplicate-emails
+
 # Write your MySQL query statement below
 SELECT Email FROM Person GROUP BY Email HAVING COUNT(Email)>1;
+
+#or
+SELECT Email FROM Person GROUP BY Email HAVING COUNT(Id)>1;
